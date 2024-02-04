62e66f9b0878aada3a886fb9c07c8b9f0072ed84
GassProd <112344231+GassProd@users.noreply.github.com>
2024-02-04 23:54:38 +0100

    Update scripts1.js

--- COPRO/javascript/scripts1.js
+++ COPRO/javascript/scripts1.js
@@ -1,17 +1,6 @@
-const mybutton = document.getElementById("mybutton");
+import Formulaire from './formulaire.js';
 
-mybutton.addEventListener("click", () => {
-  alert("Vous avez cliqué sur le bouton");
-});
+//on crée le formulaire
 
-//Voici un exemple de code JavaScript qui écoute l'événement keyup pour détecter quand l'utilisateur a appuyé sur la touche « Entrée » :
-const input = document.getElementById("myinput");
-input.addEventListener("keyup", function (event){
-  if (event.key === "Enter"){
-    alert("Vous avez appuyé sur la touche Entrée");
-  }
-  else {
-    alert(myinput.value);
-  }
-}); // Add closing parenthesis here
+export const formulaire = new Formulaire("formulaire");
 
